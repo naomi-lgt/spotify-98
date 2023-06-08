1264f8804fc5ede77e329f29ad5a10c9d51de85a
Axel LEGENTIL <axel.legentil@outlook.fr>
2023-06-08 21:01:17 +0200

    :construction: First commit, first component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className="app-container">
+        <div className="spotify-container"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
