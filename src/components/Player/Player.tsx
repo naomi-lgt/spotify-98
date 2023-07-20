feb93be9da723056aca344e751255e926e0f8785
Naomi Legentil <naomi.legentil@outlook.fr>
2023-07-20 17:42:05 +0200

    :sparkles: Player and volume slider working

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,11 +1,15 @@
 import './Player.scss';
 import songIcon from '../../assets/player/song.png'
-import cursor from '../../assets/player/cursor.png'
 import volume from '../../assets/player/volume.png'
 import {buttonDataArray} from '../../fixedData'
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 function Player() {
+    const [volumeSlider, setVolumeSlider] = useState(50);
+    const handleVolume = (e: any) => {
+        setVolumeSlider(e.target.value)
+    }
     return (
         <div className="player-container">
             <div className="player-song-info">
@@ -25,17 +29,13 @@ function Player() {
                 </div>
                 <div className="song-length">
                     <span className="length-start">0:00</span>
-                        <div className="length-player range-slider">
-                            <img src={cursor} alt="Player slider" />
-                        </div>
+                        <input type="range" id="song-slider" name="song-slider" min="0" max="100" className="length-player range-slider"></input>
                     <span className="length-end">3:00</span>
                 </div>
             </div>
             <div className="player-volume">
-                <img className="volume-icon" src={volume} alt="Change volume" />
-                <div className="volume-slider range-slider">
-                    <img src={cursor} alt="Volume slider" />
-                </div>
+                <img className="volume-icon" src={volume} alt="Change volume" onClick={() => setVolumeSlider(0)}/>
+                <input type="range" id="song-slider" name="song-slider" min="0" max="100" value={volumeSlider} className="volume-slider range-slider" onChange={(e) => handleVolume(e)}></input>
             </div>
         </div>
     )
